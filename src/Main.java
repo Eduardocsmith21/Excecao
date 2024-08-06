18c2eb175b48ac81d2f8d600276dc1982b0fa9c7
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2024-08-06 15:13:41 -0400

    Atualização

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+    }
+}
