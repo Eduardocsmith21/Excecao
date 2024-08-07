8ff711e4c4a1113b0f15152887f38a1309cf7e59
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2024-08-07 14:37:45 -0400

    Atualização parte 01

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Reserva;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException /*throws propaga exceção da linha 18, 20*/ {
         
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Número do quarto: ");
+        int numero_quarto = sc.nextInt();
+        System.out.println("Check-In (dd/mm/yyyy): ");
+        Date checkIn = (Date) sdf.parse(sc.next());
+        System.out.println("Check-Out (dd/mm/yyyy): ");
+        Date checkOut = (Date) sdf.parse(sc.next());
+
+        if(!checkOut.after(checkIn)){
+            System.out.println("Check-in");
+        }
+        else{
+
+            Reserva reserva = new Reserva(numero_quarto, checkIn, checkOut);
+            System.out.println("Reserva: " + reserva);
+        }
+        sc.close();
     }
 }
